fa5c2b705e8c79af1145b0ba5c78334145bfec8d
Serabass <is_everything@mail.ru>
2015-08-27 15:36:37 +0600

    gulpfile.js modifications

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require("gulp");
+
+var path = require('path');
+var UPLOAD = './upload';
+var SRC = path.join(UPLOAD, '_src');
+var DEST = path.join(UPLOAD);
+var THEME = 'default';
+
+var paths = {
+    admin: path.join(UPLOAD, 'admin'),
+    catalog: path.join(UPLOAD, 'catalog')
+};
+
+var js = {
+    catalog: path.join(paths.catalog, 'view', 'javascript', '**', '*.js')
+};
+
+var css = {
+    catalog: path.join(paths.catalog, 'view', 'theme', THEME, 'stylesheet', '**', '*.css')
+};
+
+gulp.task('styles', function() {
+    return gulp.src('./src/**/*.css')
+        .pipe(less())
+        .pipe(gulp.dest('./build'));
+});
+
+gulp.task('debug', function () {
+    console.log(js.catalog);
+});
