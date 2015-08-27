7c62ce812990d0a74e9e073d8fd8e10a49cb19ba
Serabass <serabass565@gmail.com>
2015-08-27 22:13:52 +0600

    gulp ftp gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
 var gulp = require("gulp");
-
-var config = require('./config.json');
-
+var gutil = require('gulp-util');
+var gftp = require('gulp-ftp');
+var config = require('./config');
+var ftp = gftp(config.ftp);
 var path = require('path');
-
 var UPLOAD = config.paths.upload;
-
 var SRC = path.join(UPLOAD, '_src');
 var DEST = path.join(UPLOAD);
 var THEME = 'default';
+var allFiles = path.join(UPLOAD, '/**');
 
 var paths = {
     admin: path.join(UPLOAD, 'admin'),
@@ -16,17 +16,28 @@ var paths = {
 };
 
 var js = {
-    catalog: path.join(paths.catalog, 'view', 'javascript', '**', '*.js')
+    catalog: path.join(paths.catalog, 'view/javascript/**/*.js')
 };
 
 var css = {
-    catalog: path.join(paths.catalog, 'view', 'theme', THEME, 'stylesheet', '**', '*.css')
+    catalog: path.join(paths.catalog, 'view/theme', THEME, 'stylesheet/**/*.css')
 };
 
 gulp.task('styles', function() {
     return gulp.src('./src/**/*.css')
         .pipe(less())
         .pipe(gulp.dest('./build'));
+});
+
+gulp.task('ftp', function() {
+
+    return gulp.src(allFiles)
+        .pipe(ftp)
+        .pipe(gutil.noop());
+});
+
+gulp.task('watch', function () {
+    gulp.watch(allFiles, ['ftp']);
 });
 
 gulp.task('debug', function () {
