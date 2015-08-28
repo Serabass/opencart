ab6ad0ccfe26faf19489143c025b76a181068d4b
Serabass <serabass565@gmail.com>
2015-08-28 23:07:59 +0600

    main content now in _src dest is an upload

---
+++ _src/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://madmart.kz/admin/');
+define('HTTP_CATALOG', 'http://madmart.kz/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://madmart.kz/admin/');
+define('HTTPS_CATALOG', 'http://madmart.kz/');
+
+// DIR
+define('DIR_APPLICATION', 'E:/OpenServer/domains/madmart.kz/opencart/upload/admin/');
+define('DIR_SYSTEM', 'E:/OpenServer/domains/madmart.kz/opencart/upload/system/');
+define('DIR_LANGUAGE', 'E:/OpenServer/domains/madmart.kz/opencart/upload/admin/language/');
+define('DIR_TEMPLATE', 'E:/OpenServer/domains/madmart.kz/opencart/upload/admin/view/template/');
+define('DIR_CONFIG', 'E:/OpenServer/domains/madmart.kz/opencart/upload/system/config/');
+define('DIR_IMAGE', 'E:/OpenServer/domains/madmart.kz/opencart/upload/image/');
+define('DIR_CACHE', 'E:/OpenServer/domains/madmart.kz/opencart/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', 'E:/OpenServer/domains/madmart.kz/opencart/upload/system/storage/download/');
+define('DIR_LOGS', 'E:/OpenServer/domains/madmart.kz/opencart/upload/system/storage/logs/');
+define('DIR_MODIFICATION', 'E:/OpenServer/domains/madmart.kz/opencart/upload/system/storage/modification/');
+define('DIR_UPLOAD', 'E:/OpenServer/domains/madmart.kz/opencart/upload/system/storage/upload/');
+define('DIR_CATALOG', 'E:/OpenServer/domains/madmart.kz/opencart/upload/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'madmart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ _src/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://madmart.kz/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://madmart.kz/');
+
+// DIR
+define('DIR_APPLICATION', 'E:/OpenServer/domains/madmart.kz/opencart/upload/catalog/');
+define('DIR_SYSTEM', 'E:/OpenServer/domains/madmart.kz/opencart/upload/system/');
+define('DIR_LANGUAGE', 'E:/OpenServer/domains/madmart.kz/opencart/upload/catalog/language/');
+define('DIR_TEMPLATE', 'E:/OpenServer/domains/madmart.kz/opencart/upload/catalog/view/theme/');
+define('DIR_CONFIG', 'E:/OpenServer/domains/madmart.kz/opencart/upload/system/config/');
+define('DIR_IMAGE', 'E:/OpenServer/domains/madmart.kz/opencart/upload/image/');
+define('DIR_CACHE', 'E:/OpenServer/domains/madmart.kz/opencart/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', 'E:/OpenServer/domains/madmart.kz/opencart/upload/system/storage/download/');
+define('DIR_LOGS', 'E:/OpenServer/domains/madmart.kz/opencart/upload/system/storage/logs/');
+define('DIR_MODIFICATION', 'E:/OpenServer/domains/madmart.kz/opencart/upload/system/storage/modification/');
+define('DIR_UPLOAD', 'E:/OpenServer/domains/madmart.kz/opencart/upload/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'madmart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
